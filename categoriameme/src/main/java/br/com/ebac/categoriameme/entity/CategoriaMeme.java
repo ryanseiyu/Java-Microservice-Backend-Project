ab1a5f7aae7575867731e08df5113ac508a42320
Ryan Seiyu <ryan_seiyu@hotmail.com>
2023-10-20 16:08:14 -0300

    Projeto final

---
+++ categoriameme/src/main/java/br/com/ebac/categoriameme/entity/CategoriaMeme.java
@@ -0,0 +1,73 @@
+package br.com.ebac.categoriameme.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CategoriaMeme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_categoria_meme")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private Date dataCadastro;
+
+    @Column(name = "usuario_id", nullable = false)
+    private Long usuarioId;
+
+    public CategoriaMeme() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+}
